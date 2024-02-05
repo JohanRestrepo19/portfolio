9b4a4c622e8af6d9d8c3e01b10fc342a98c10fa9
Johan Restrepo <62318029+JohanRestrepo19@users.noreply.github.com>
2024-02-05 12:05:59 -0500

    chore: add basic project page structure.

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -1,8 +1,53 @@
-type Props = {
+import Link from 'next/link';
+import Container from '@/components/Container';
+import Heading from '@/components/Heading';
+import Paragraph from '@/components/Paragraph';
+import { Badge } from '@/components/ui/badge';
+
+// export function generateStaticParams() {
+//   return allProjects.map(project => ({
+//     slug: project._raw.flattenedPath,
+//   }));
+// }
+//
+// export function generateMetadata({ params }: PageProps): Metadata {
+//   const project = allProjects.find(
+//     project => project._raw.flattenedPath === params.slug,
+//   );
+//
+//   if (!project) throw new Error(`Project not found for slug: ${params.slug}`);
+//   return {
+//     title: project.title,
+//   };
+// }
+
+type PageProps = {
   params: { slug: string };
 };
 
-export default function Project({ params }: Props) {
-  console.log({ params });
-  return <h1>Project page for: {params.slug}</h1>;
+export default function ProjectPage({}: PageProps) {
+  return (
+    <Container>
+      <Heading as="h2" className="text-center">
+        title
+      </Heading>
+      <Paragraph>description</Paragraph>
+      <ul className="my-4 ml-4">
+        <li>
+          <Badge className="mr-2 rounded-md">Stack</Badge>
+          stack
+        </li>
+
+        <li>
+          <Badge className="mr-2 rounded-md">Source</Badge>
+          source
+        </li>
+
+        <li>
+          <Badge className="mr-2 rounded-md">Website</Badge>
+          <Link href="#">Website</Link>
+        </li>
+      </ul>
+    </Container>
+  );
 }
