544a8bedf3418d8dcb3bedb01af1a48d098a56e6
Johan Restrepo <62318029+JohanRestrepo19@users.noreply.github.com>
2024-02-07 11:49:31 -0500

    chore: prepare dictionaries.

--- src/i18n/en/index.ts
+++ src/i18n/en/index.ts
@@ -1,5 +1,8 @@
 import turistriadaEn from '@/i18n/en/projects/turistriada.json';
-import turistriadaLandingEn from '@/i18n/en/projects/turistrida-landing.json';
-import type { Project } from '@/lib/types';
+import turistriadaLandingEn from '@/i18n/en/projects/turistriada-landing.json';
+import EnDictionary from '@/i18n/en/dictionary.json';
+
+import type { Dictionary, Project } from '@/lib/types';
 
 export const allEnProjects: Project[] = [turistriadaEn, turistriadaLandingEn];
+export const dictionary: Dictionary = EnDictionary;
---
+++ src/i18n/es/index.ts
@@ -0,0 +1,8 @@
+import turistriadaEs from '@/i18n/es/projects/turistriada.json';
+import turistriadaLandingEs from '@/i18n/es/projects/turistriada-landing.json';
+import EsDictionary from '@/i18n/es/dictionary.json';
+
+import type { Dictionary, Project } from '@/lib/types';
+
+export const allEsProjects: Project[] = [turistriadaEs, turistriadaLandingEs];
+export const dictionary: Dictionary = EsDictionary;
