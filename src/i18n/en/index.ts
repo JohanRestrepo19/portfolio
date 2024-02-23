a614a9f2de7aad438367bebb81570b3bbddaf5d9
Johan Restrepo <62318029+JohanRestrepo19@users.noreply.github.com>
2024-02-22 22:06:37 -0500

    feat: add Banker's algorithm project.

--- src/i18n/en/index.ts
+++ src/i18n/en/index.ts
@@ -1,7 +1,8 @@
 import turistriadaEn from '@/i18n/en/projects/turistriada.json';
+import bankersAlgorithmEn from '@/i18n/en/projects/bankers-algorithm.json'
 import EnDictionary from '@/i18n/en/dictionary.json';
 
 import type {Dictionary, Project} from '@/lib/types';
 
-export const allEnProjects: Project[] = [turistriadaEn];
+export const allEnProjects: Project[] = [turistriadaEn, bankersAlgorithmEn];
 export const dictionary: Dictionary = EnDictionary;
--- src/i18n/es/index.ts
+++ src/i18n/es/index.ts
@@ -1,7 +1,8 @@
 import turistriadaEs from '@/i18n/es/projects/turistriada.json';
+import bankersAlgorithmEs from '@/i18n/es/projects/bankers-algorithm.json'
 import EsDictionary from '@/i18n/es/dictionary.json';
 
 import type {Dictionary, Project} from '@/lib/types';
 
-export const allEsProjects: Project[] = [turistriadaEs];
+export const allEsProjects: Project[] = [turistriadaEs, bankersAlgorithmEs];
 export const dictionary: Dictionary = EsDictionary;
